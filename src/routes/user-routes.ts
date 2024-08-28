d63440f9a792f29c2c00e1473b1938309cc6bd9e
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-08-28 10:45:13 -0300

    all routes user-routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -6,6 +6,7 @@ import { IParamsId, UserCreateBody, UserUpdateBody } from '../types/user.types';
 async function userRoutes(fastify:FastifyInstance) {
   fastify.get('/', userFindAll);
   fastify.get<{ Params: IParamsId }>('/findbyidsimple/:id?', userFindById);
+  fastify.get('/findById/:id?', { preHandler: authPlugin }, userFindById)
   fastify.post<{ Body: UserCreateBody }>('/create', userCreate);
   fastify.patch<{ Body: UserUpdateBody, Params: IParamsId }>('/findByIdUpdate/:id?', { preHandler: authPlugin }, userUpdate);
   fastify.delete<{ Params: IParamsId }>('/findByIdDelete/:id?', userRemove)
